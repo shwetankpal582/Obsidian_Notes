50f1bca423341d5a0c3bf2c09486ea1515b470f2
Shwetank Pal <shwetankpal582@gmail.com>
2025-01-26 21:37:56 +0530

    vault backup: 2025-01-26 21:37:55

--- Backend/Nodejs/Code/server.js
+++ Backend/Nodejs/Code/server.js
@@ -1,2 +1,11 @@
 const express = require('express');
-const app = 
+const app = express();
+// const db = require('./db');
+
+app.get('/', function (req,res){
+    res.send("Welcome to my hotel")
+})
+
+app.listen(3000, () => {
+    console.log("listening to port 3000");
+})
