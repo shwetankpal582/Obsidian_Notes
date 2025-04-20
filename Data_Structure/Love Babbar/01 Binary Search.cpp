747551e03327823f9fe50be839e4d82a618f0e72
Shwetank Pal <shwetankpal582@gmail.com>
2025-04-20 14:58:24 +0530

    vault backup: 2025-04-20 14:58:24

--- Data_Structure/Love Babbar/01 Binary Search.cpp
+++ Data_Structure/Love Babbar/01 Binary Search.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
-    
+    int BinarySearch(int arr, int size, int key){
+        int start = 0;
+        int end = size-1;
+
+        int mid = (start+end)/2;
+
+        while( start <= end ){
+
+            if( arr[mid] == key ){
+                return mid;
+            }
+
+            // Right   (start=mid+1)
+            if( key > arr[mid] ){
+
+                start = mid+1;
+
+            }
+        }
+    }
 
     int main() {
     
