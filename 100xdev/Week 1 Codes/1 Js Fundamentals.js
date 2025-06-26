4824a25f88572ce2e243fccfda056e9a4fe8af69
Shwetank Pal <shwetankpal582@gmail.com>
2025-06-26 22:48:04 +0530

    vault backup: 2025-06-26 22:48:03

--- 100xdev/Week 1 Codes/1 Js Fundamentals.js
+++ 100xdev/Week 1 Codes/1 Js Fundamentals.js
@@ -128,9 +128,12 @@
 //     prop2: 9
 // }
 
-const ages = [21,22,23,24,25];
-for(let i=0;i<=ages.length;i++){
-    if (ages[i]%2==0){
-        console.log(ages[i]);
-    }
-}
+// const ages = [21,22,23,24,25];
+// for(let i=0;i<=ages.length;i++){
+//     if (ages[i]%2==0){
+//         console.log(ages[i]);
+//     }
+// }
+
+// Object
+
