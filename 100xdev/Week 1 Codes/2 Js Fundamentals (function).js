fdbfc1eae68c150a27c26deb735c5ae11863dd9d
Shwetank Pal <shwetankpal582@gmail.com>
2025-06-30 19:39:13 +0530

    vault backup: 2025-06-30 19:39:13

--- 100xdev/Week 1 Codes/2 Js Fundamentals (function).js
+++ 100xdev/Week 1 Codes/2 Js Fundamentals (function).js
@@ -5,14 +5,15 @@
 // const sumval = sum(5,8);
 // console.log(sumval);
 
-function CutIt(str, startIndex, endIndex) {
-    let newStr = "Shwetank";
-    for (let i = 0; i < str.length; i++) {
-        if (i >= startIndex && i <= endIndex) {
-            newStr = newStr + str[i];
-        }
-    }
-    return newStr;
-}
-const value = "Shwetank";
-console.log(CutIt(value,2,5 ));
+// function CutIt(str, startIndex, endIndex) {
+//     let newStr = "Shwetank";
+//     for (let i = 0; i < str.length; i++) {
+//         if (i >= startIndex && i <= endIndex) {
+//             newStr = newStr + str[i];
+//         }
+//     }
+//     return newStr;
+// }
+// const value = "Shwetank";
+// console.log(CutIt(value,2,5 ));
+
