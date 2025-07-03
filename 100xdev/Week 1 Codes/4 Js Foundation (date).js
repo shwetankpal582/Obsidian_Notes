04e2b351a5865b5b067881eddb0546be76d7bac0
Shwetank Pal <shwetankpal582@gmail.com>
2025-07-03 23:29:35 +0530

    vault backup: 2025-07-03 23:29:35

--- 100xdev/Week 1 Codes/4 Js Foundation (date).js
+++ 100xdev/Week 1 Codes/4 Js Foundation (date).js
@@ -1,5 +1,16 @@
-const currentDate = new Date();
-console.log(currentDate.getMonth() + 1); // Months are 0-indexed, so we add 1 to get the correct month number
-console.log(currentDate.getDate());
-console.log(currentDate.getFullYear());
-console.log(currentDate.getHours());
+// const currentDate = new Date();
+// console.log(currentDate.getMonth() + 1); // Months are 0-indexed, so we add 1 to get the correct month number
+// console.log(currentDate.getDate());
+// console.log(currentDate.getFullYear());
+// console.log(currentDate.getHours());
+
+function calculatesum() {
+    let a = 0;
+    for (let i = 0; i < 100000000; i++) {
+        a += i[i];
+    }   
+    return a;
+}
+
+const beforedate = new Date();
+const BeforeTimeInMs
