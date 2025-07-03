56e3cbfab0a67f605ab9bc95d0e09b52c420e2a6
Shwetank Pal <shwetankpal582@gmail.com>
2025-07-03 18:58:36 +0530

    vault backup: 2025-07-03 18:58:35

--- 100xdev/Week 1 Codes/3 Js Fundamentals (map,filter,find,sort).js
+++ 100xdev/Week 1 Codes/3 Js Fundamentals (map,filter,find,sort).js
@@ -1,20 +1,25 @@
-// Map, Filter, Find, Sort
+// // Map, Filter, Find, Sort
  
-const dog = {
-    name : "dog",
-    legcount : 4,
-    speak : "bhow bhow",
-}
+// const dog = {
+//     name : "dog",
+//     legcount : 4,
+//     speak : "bhow bhow",
+// }
 
-const cat = {
-    name : "cat",
-    legcount : 4,
-    speak : "meow meow",
-}
+// const cat = {
+//     name : "cat",
+//     legcount : 4,
+//     speak : "meow meow",
+// }
 
 
-function printani(){
+// function printani(animal){
+//     console.log("animal" + " " + animal["name"])  // method 1
+// // console.log("animal" + " " + animal.cat.name)  // method 2
+// };
+// printani(dog);
+// printani(cat);
+
+class Animal {
     
 }
-console.log("animal" + " " + dog["name"])  // method 1
-console.log("animal" + " " + dog.name)  // method 2
