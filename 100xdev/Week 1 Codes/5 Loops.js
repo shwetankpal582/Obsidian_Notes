ea365d10a0d64398791db22216f6d701f1a8cf77
Shwetank Pal <shwetankpal582@gmail.com>
2025-07-04 13:59:17 +0530

    vault backup: 2025-07-04 13:59:16

--- 100xdev/Week 1 Codes/5 Loops.js
+++ 100xdev/Week 1 Codes/5 Loops.js
@@ -1,1 +1,11 @@
 // Callback Functions
+function square(n){
+    return n*n;
+}
+function sumofaquare(a, b) {
+    const val1 = square(a);
+    const val2 = square(b);
+
+    return val1 + val2;
+}
+console.log(sumofaquare(2, 3)); 
